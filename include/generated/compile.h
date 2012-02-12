4ee8675af202485efe8b6ff5615362f8b512c860
Bdaman80 <bdaman80@gmail.com>
2012-02-12 09:42:18 -0600

    Add Smartass2 Gov and Sync

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Nov 13 13:04:36 CST 2011"
+#define LINUX_COMPILE_TIME "13:04:36"
+#define LINUX_COMPILE_BY "bdaman"
+#define LINUX_COMPILE_HOST "Height03"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
